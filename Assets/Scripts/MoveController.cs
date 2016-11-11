28df1d942f9de8cfa8253256255800f29630a7a4
falcon <imadake398yen@gmail.com>
2016-11-11 17:54:28 +0900

    動きをつけるスクリプト

---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveController : MonoBehaviour {
+
+	public Transform target;
+	private float 
+		speed = 8.0f,
+		maxSpeed = 3.0f;
+
+	void Update () {
+		if (target != null) {
+			Vector3 v = Vector3.Lerp(
+				transform.position, 
+				target.position, 
+				Time.deltaTime * speed) - transform.position;
+			if (v.magnitude > maxSpeed) v = v.normalized * maxSpeed;
+			transform.position += v;
+
+			// rotation
+			transform.rotation = Quaternion.Lerp(
+				transform.rotation,
+				target.rotation,
+				Time.deltaTime * speed);
+		}
+	}
+}
