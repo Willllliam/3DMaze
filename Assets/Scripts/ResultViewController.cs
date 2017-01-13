8e6df821361f76f9cb197b4fae1c60d381a348c3
william <wvaardt@hotmail.com>
2017-01-13 19:20:19 +0900

    スクリプト作ったお

---
+++ Assets/Scripts/ResultViewController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResultViewController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
