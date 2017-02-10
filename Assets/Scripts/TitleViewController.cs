d3ff1a25e7e05054143e9f82c6c6788aae3e0cbe
falcon <imadake398yen@gmail.com>
2017-02-10 18:25:12 +0900

    データ削除ボタンつけた

--- Assets/Scripts/TitleViewController.cs
+++ Assets/Scripts/TitleViewController.cs
@@ -4,17 +4,27 @@ using System.Collections;
 
 public class TitleViewController : MonoBehaviour {
 
-	public Button startButton;
+	public Button 
+		startButton,
+		deleteButton;
 
 	void Start () {
 		startButton.onClick.AddListener(PushStartButton);
+		deleteButton.onClick.AddListener(PushDeleteButton);
 	}
 	
-	private void PushStartButton () {
+	private void PushStartButton () 
+	{
 		if (GameManager.instance.loaded) 
 		{
 			ViewManager.instance.ChangeView(Const.ViewType.StageSelect);
 			ViewManager.instance.stageSelectView.ViewStart();
 		}
+	}
+
+	private void PushDeleteButton () 
+	{
+		PlayerPrefs.DeleteAll();
+		UnityEngine.SceneManagement.SceneManager.LoadScene("Original game");
 	}
 }
